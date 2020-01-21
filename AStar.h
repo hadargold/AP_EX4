fab51bf6ea59ae4b468a8923f853819a7c6c176c
hadargold <50302613+hadargold@users.noreply.github.com>
2020-01-21 22:06:55 +0200

    21.1

---
+++ AStar.h
@@ -0,0 +1,19 @@
+#ifndef SERVERAPPLICATION_ASTARBLOG_H
+#define SERVERAPPLICATION_ASTARBLOG_H
+
+#include "ISearcher.h"
+#include <map>
+
+namespace server_side {
+
+
+    class AStar : public ISearcher {
+
+        SearchAlgo searchAlgo;
+
+    public:
+        SearchAlgo search(ISearchable *searchable) override;
+    };
+}
+
+#endif //SERVERAPPLICATION_ASTARBLOG_H
