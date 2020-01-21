67db20b89488f2079dd879eac4678f5cf1b9b448
hadargold <50302613+hadargold@users.noreply.github.com>
2020-01-21 22:00:48 +0200

    21.1

---
+++ DFS.h
@@ -0,0 +1,21 @@
+#ifndef SERVERAPPLICATION_DFS_H
+#define SERVERAPPLICATION_DFS_H
+
+#include "ISearcher.h"
+
+namespace server_side {
+
+    class DFS : public ISearcher {
+
+        SearchAlgo searchAlgo;
+        State* goalState;
+
+    public:
+
+        SearchAlgo search(ISearchable *searchable) override;
+        bool recursiveSearch(State* currentState);
+
+    };
+}
+
+#endif //SERVERAPPLICATION_DFS_H
