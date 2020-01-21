ab92396c6b759c95f5740302b287d854eed1ef26
hadargold <50302613+hadargold@users.noreply.github.com>
2020-01-21 22:03:50 +0200

    21.1

---
+++ BFS.h
@@ -0,0 +1,18 @@
+#ifndef SERVERAPPLICATION_BFS_H
+#define SERVERAPPLICATION_BFS_H
+
+
+#include "ISearcher.h"
+#include <queue>
+
+namespace server_side {
+
+
+    class BFS : public ISearcher {
+
+    public:
+        SearchAlgo search(ISearchable *searchable) override;
+    };
+}
+
+#endif //SERVERAPPLICATION_BFS_H
