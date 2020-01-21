7a5bd69a33522a5646251cd244fa7bc44e4b9e5c
hadargold <50302613+hadargold@users.noreply.github.com>
2020-01-21 22:09:13 +0200

    21.1

---
+++ ISearchable.h
@@ -0,0 +1,24 @@
+#ifndef SERVERAPPLICATION_ISEARCHABLE_H
+#define SERVERAPPLICATION_ISEARCHABLE_H
+
+#include <vector>
+#include "State.h"
+
+namespace server_side {
+
+    using std::vector;
+
+    class ISearchable {
+
+    public:
+
+        virtual State *getInitialState() = 0;
+
+        virtual State *getGoalState() = 0;
+
+        virtual std::vector<State *> getAllPossibleStates(State *s) = 0;
+
+    };
+}
+
+#endif //SERVERAPPLICATION_ISEARCHABLE_H
