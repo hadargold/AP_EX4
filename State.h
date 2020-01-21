d9c4ee89b3d3331493daafba2493635df04c409b
hadargold <50302613+hadargold@users.noreply.github.com>
2020-01-21 22:00:08 +0200

    21.1

---
+++ State.h
@@ -0,0 +1,56 @@
+#ifndef SERVERAPPLICATION_STATE_H
+#define SERVERAPPLICATION_STATE_H
+
+#include <string>
+#include <vector>
+#include "Enumes.h"
+
+namespace server_side {
+    
+    class State
+    {
+
+    public:
+
+        State *top;
+        State *bottom;
+        State *left;
+        State *right;
+
+        int weigth;
+        bool isInfinity;
+        bool isMarked;
+        Direction commingFrom;
+        unsigned int row_index;
+        unsigned int col_index;
+
+        State(int weigth) : top(NULL), bottom(NULL), left(NULL), right(NULL), weigth(weigth), isMarked(false) {}
+
+        std::vector<State *> getNeighbors() {
+            std::vector<State *> ret;
+
+            if (bottom != NULL)
+                ret.push_back(bottom);
+            if (top != NULL)
+                ret.push_back(top);
+            if (left != NULL)
+                ret.push_back(left);
+            if (right != NULL)
+                ret.push_back(right);
+            return ret;
+        }
+    };
+
+
+    struct SearchAlgo {
+
+        std::string shortestPathRoute;
+
+        int shortestPathWeight;
+
+        int developedVertices;
+
+    };
+}
+
+#endif //SERVERAPPLICATION_STATE_H
