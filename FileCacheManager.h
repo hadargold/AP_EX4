f756a9cd37fe52e0b4a5feb2409bc4463a54b209
hadargold <50302613+hadargold@users.noreply.github.com>
2020-01-22 21:24:29 +0200

    22.1 21:20

---
+++ FileCacheManager.h
@@ -0,0 +1,41 @@
+#ifndef SERVERAPPLICATION_FILECACHEMANAGER_H
+#define SERVERAPPLICATION_FILECACHEMANAGER_H
+
+#include <map>
+#include <mutex>
+
+#include "ICacheManager.h"
+#include "Utils.h"
+#define CACHE "cache_file.txt"
+#define SPLIT_CHAR '$'
+
+namespace server_side{
+
+    class FileCacheManager : public ICacheManager {
+
+        std::mutex mut;
+
+    public:
+
+        bool isExistsInCache(std::string key) override;
+
+        std::string loadFromCache(std::string key) override;
+
+        void saveToCache(std::string key, std::string solution) override;
+
+        // appends to a given file name. creates him if he haven't been created yet
+
+        static void appendToFile(std::string fileName, std::string dataToAppend){
+            std::fstream stream;
+            stream.open(fileName, std::ios::app | std::ios::ate);
+
+            stream << dataToAppend;
+
+            stream.flush();
+            stream.close();
+        };
+
+    };
+}
+
+#endif //SERVERAPPLICATION_FILECACHEMANAGER_H
