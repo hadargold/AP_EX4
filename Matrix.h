0518bff79a43c3410db851824c1ad71e1986d779
hadargold <50302613+hadargold@users.noreply.github.com>
2020-01-23 12:33:27 +0200

    23.1

---
+++ Matrix.h
@@ -0,0 +1,32 @@
+#ifndef SERVERAPPLICATION_MATRIX_H
+#define SERVERAPPLICATION_MATRIX_H
+
+#include "ISearchable.h"
+
+using std::vector;
+
+namespace server_side {
+    
+    class Matrix : public ISearchable {
+        
+        State *initialState;
+        State *goalState;
+
+    public:
+
+        vector<vector<State>> matrix;
+
+        Matrix(vector<vector<int>> givenMatrix, std::pair<int, int> src, std::pair<int, int> dst);
+
+        State *getInitialState() override;
+
+        State *getGoalState() override;
+
+        vector<State *> getAllPossibleStates(State *s) override;
+
+    };
+
+}
+
+
+#endif //SERVERAPPLICATION_MATRIX_H
